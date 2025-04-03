62a3820935b5198529cc87f582394a02b2a95a75
Vittor Patricio <vittorpatricio26@gmail.com>
2025-04-03 15:19:27 -0300

    gradlew clean

--- android/app/src/main/java/com/coffeeapp/MainApplication.kt
+++ android/app/src/main/java/com/coffeeapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.coffeeapp
+package com.qrcode
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/CoffeeAppTests/CoffeeAppTests.m
+++ ios/CoffeeAppTests/CoffeeAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface CoffeeAppTests : XCTestCase
+@interface qrcodeTests : XCTestCase
 
 @end
 
-@implementation CoffeeAppTests
+@implementation qrcodeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
